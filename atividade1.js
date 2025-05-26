d4e2bb65d34c62eae734615043312ff090217c22
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2025-05-26 16:19:37 +0000

    ubfj

---
+++ atividade1.js
@@ -0,0 +1,11 @@
+let idade = 14;
+let ano = 365;
+let hora = 24;
+let minuto = 60;
+let segundo = 60;
+let diasvividos = idade * ano;
+let minutosvividos = diasvividos * horas
+console.log ("Olá" + nome)
+console.log (idade * ano);
+console.log (diasvividos * horas);
+console.log ()
